dc3fef86a0fa15c6113d438600839359d2080222
Bijay Shrestha <bijayashrestha929@gmail.com>
2025-06-14 22:21:55 +0545

    yeah
    yeah
    
    Co-Authored-By: Bijay Shrestha <124568970+Bijay-Shre-S-tha@users.noreply.github.com>

--- Destructor/obj/Debug/net9.0/Destructor.AssemblyInfo.cs
+++ Destructor/obj/Debug/net9.0/Destructor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Destructor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f78081c0b1c985bbe8c4eb9d22b9ce3672a04414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+729cf26cf5b6f8108ccc11287ebe1fda62e2e3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Destructor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Destructor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ParameterizedConstructor/obj/Debug/net9.0/ParameterizedConstructor.AssemblyInfo.cs
+++ ParameterizedConstructor/obj/Debug/net9.0/ParameterizedConstructor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParameterizedConstructor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f78081c0b1c985bbe8c4eb9d22b9ce3672a04414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+729cf26cf5b6f8108ccc11287ebe1fda62e2e3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParameterizedConstructor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParameterizedConstructor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
